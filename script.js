131b28b17fd3b83f83a76dfb8bb4ddfa36d4e0bc
Thomas Blachowiak <blachowiak.thomas@outlook.fr>
2023-04-30 18:41:40 +0200

    creat file script.js, download dice pictures

---
+++ script.js
@@ -0,0 +1,11 @@
+let randomNumber = 0;
+let roundScore = 0;
+let activePlayer = 0;
+let scores = [0, 0];
+
+const dice = document.getElementById("diceNumber");
+const roll = document.getElementById("rollDice");
+const hold = document.getElementById("hold");
+const newGame = document.getElementById("replay");
+const player1 = document.getElementById("playerOne");
+const player2 = document.getElementById("playerTwo");

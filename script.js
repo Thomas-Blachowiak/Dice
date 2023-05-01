20d200369ee140a767fdc4c9373142b1aa7f9f5b
Thomas Blachowiak <blachowiak.thomas@outlook.fr>
2023-05-01 18:42:57 +0200

    create file script.js, create variable, create random number, display dice and round score

--- script.js
+++ script.js
@@ -9,3 +9,24 @@ const hold = document.getElementById("hold");
 const newGame = document.getElementById("replay");
 const player1 = document.getElementById("playerOne");
 const player2 = document.getElementById("playerTwo");
+
+// Create a random number   // Display dice  // Round score
+const rollDice = () => {
+  randomNumber = Math.floor(Math.random() * 6) + 1;
+  diceNumber.innerHTML = `<img class="dice" src="./images/dice-${randomNumber}.png" alt="dice ${randomNumber}">`;
+  if (randomNumber !== 1) {
+    roundScore += randomNumber;
+    document.querySelector(`#current-${activePlayer}`).textContent = roundScore;
+  } else {
+    changePlayer();
+  }
+};
+
+// Change player
+const changePlayer = () => {
+  roundScore = 0;
+  document.querySelector(`#current-${activePlayer}`).textContent = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player1.classList.toggle("active-player");
+  player2.classList.toggle("active-player");
+};

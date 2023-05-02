bf8cdc5243bfac3e184aab2c71d5fa726b8d418a
Thomas Blachowiak <blachowiak.thomas@outlook.fr>
2023-05-02 11:35:56 +0200

    add highlight the player and disable buttons at game end

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ const player2 = document.getElementById("playerTwo");
 // Create a random number   // Display dice  // Round score
 const rollDice = () => {
   randomNumber = Math.floor(Math.random() * 6) + 1;
-  diceNumber.innerHTML = `<img class="dice" src="./images/dice-${randomNumber}.png" alt="dice ${randomNumber}">`;
+  diceNumber.innerHTML = `<img class="dice" src="./image/dice-${randomNumber}.png" alt="dice ${randomNumber}">`;
   if (randomNumber !== 1) {
     roundScore += randomNumber;
     document.querySelector(`#current-${activePlayer}`).textContent = roundScore;
@@ -22,11 +22,57 @@ const rollDice = () => {
   }
 };
 
+// Highlight the player
+const highlightActivePlayerName = () => {
+  if (activePlayer === 0) {
+    player1.style.fontSize = "200%";
+    player2.style.fontSize = "120%";
+  } else {
+    player2.style.fontSize = "200%";
+    player1.style.fontSize = "120%";
+  }
+};
+
 // Change player
 const changePlayer = () => {
   roundScore = 0;
   document.querySelector(`#current-${activePlayer}`).textContent = 0;
   activePlayer = activePlayer === 0 ? 1 : 0;
+  highlightActivePlayerName();
   player1.classList.toggle("active-player");
   player2.classList.toggle("active-player");
 };
+
+// Hold the score
+const holdScore = () => {
+  scores[activePlayer] += roundScore;
+  document.querySelector(`#score-${activePlayer}`).textContent = scores[activePlayer];
+  if (scores[activePlayer] >= 100) {
+    document.querySelector(`.playerName-${activePlayer}`).classList.add("winner-player");
+    document.querySelector(`.playerName-${activePlayer}`).innerHTML = `<p>Vous avez gagné !</p>`;
+    stopRolling();
+  } else {
+    changePlayer();
+  }
+};
+
+// Disable buttons at game end
+const stopRolling = () => {
+  roll.disabled = true;
+  hold.disabled = true;
+  
+  hold.classList.remove("inactive-button");
+  hold.classList.remove("inactive-button");
+
+  dice.style.opacity = "0.5";
+};
+
+// New game
+const replay = () => {
+  document.location.reload();
+};
+
+// click events
+roll.addEventListener("click", rollDice, false);
+hold.addEventListener("click", holdScore, false);
+newGame.addEventListener("click", replay, false);
